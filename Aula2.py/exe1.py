d1aeac0ccc7cbbf697ac5cdf0540c418e041dc49
LuisFelipeR05 <luisfelipersantos05@outlook.com>
2026-05-20 20:03:43 -0300

    20/05/26

---
+++ Aula2.py/exe1.py
@@ -0,0 +1,8 @@
+#Operações logicas em python
+numer1 = 10
+numer2 = 5
+
+print(numer1 + numer2)
+print(numer1 - numer2)
+print(numer1 * numer2)
+print(numer1 / numer2)
---
+++ Aula2.py/exe2.py
@@ -0,0 +1,13 @@
+print(10 == 10) #COMPARAÇÃO 
+
+#OPERADOR AND as diuas conciões verdadeiras
+idade = 20
+tem_carteira = True #bool
+print(idade >= 18 and tem_carteira) #TRUE   
+20 >= 18
+
+
+#operadosres logicos Or
+dia = "quarta-feira"
+print(dia == "segunda-feira" or dia == "quarta-feira") #TRUE
+
